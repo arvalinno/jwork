b3f48ea808f8cddc854556452e65fa2f360c064d
Arvalinno <arvalinno@gmail.com>
2021-04-03 09:23:10 +0700

    Modul 4 - Post Test - PostTest1

---
+++ jwork/BankPayment.java
@@ -0,0 +1,62 @@
+
+/**
+ * Write a description of class BankPayment here.
+ *
+ * @author Arvalinno
+ * @version 03.04.2021
+ */
+public class BankPayment extends Invoice
+{
+    // instance variables - replace the example below with your own
+    private static final PaymentType PAYMENT_TYPE = PaymentType.BankPayment;
+    private int adminFee;
+    
+        /**
+     * Constructor for objects of class BankPayment
+     */
+    public BankPayment(int id, Job job, String date, Jobseeker jobseeker, InvoiceStatus invoiceStatus)
+    {
+        super(id, job, date, jobseeker, invoiceStatus); 
+    }
+
+    public BankPayment(int id, Job job, String date, Jobseeker jobseeker, InvoiceStatus invoiceStatus, int adminFee){
+        super(id, job, date, jobseeker, invoiceStatus);
+        this.setAdminFee(adminFee);
+    }
+    
+    @Override
+    public PaymentType getPaymentType(){
+        return PAYMENT_TYPE;
+    }
+    
+    public int getAdminFee() {
+        return adminFee;
+    }
+    
+    public void setAdminFee(int adminFee) {
+        this.adminFee = adminFee;
+    }
+    
+    @Override
+    public void setTotalFee() {
+         if (adminFee > 0) {
+            super.totalFee = getJob().getFee() - adminFee;
+        } else {
+            super.totalFee = getJob().getFee();
+        }    
+    }
+    
+    @Override
+    public void printData() {
+       System.out.println("===================== INVOICE =====================");
+        System.out.println("ID: " + getId());
+        System.out.println("Job: " + getJob().getName());
+        System.out.println("Date: " + getDate());
+        System.out.println("Job Seeker: " + getJobseeker().getName());
+        System.out.println("Admin Fee: " + adminFee);
+        setTotalFee();
+        System.out.println("Total Fee: " + getTotalFee());
+        System.out.println("Status: " + getInvoiceStatus());
+        System.out.println("Payment Type: " + PAYMENT_TYPE);
+    }
+}
--- jwork/JWork.java
+++ jwork/JWork.java
@@ -24,7 +24,7 @@ public class JWork
         Job job1 = new Job(1, "Junior Data Analyst", recruiter1, 5000000, JobCategory.DataAnalyst);
         Jobseeker jobseeker1 = new Jobseeker(1, "Tony", "Tony@gmail.com", "password", "27/3/2021");
         
-        Bonus bonusA = new Bonus(7, "OOPASIK", 5000000, 4500000, true);
+        /* Bonus bonusA = new Bonus(7, "OOPASIK", 5000000, 4500000, true);
         Bonus bonusB = new Bonus(8, "ASIKNIH", 7000000, 6500000, true);
         
         EwalletPayment payment1 = new EwalletPayment(1, job1, "28 Maret 2021", jobseeker1, InvoiceStatus.OnGoing);
@@ -33,6 +33,13 @@ public class JWork
            
         payment1.printData();
         payment2.printData();
-        payment3.printData();
+        payment3.printData(); */
+        
+        BankPayment payment1 = new BankPayment(5, job1, "02/4/2020", jobseeker1, InvoiceStatus.Finished);
+
+        BankPayment payment2 = new BankPayment(6, job1, "03/4/2020", jobseeker1, InvoiceStatus.Finished, 30000);
+
+        payment1.printData();
+        payment2.printData();
     }
 }
